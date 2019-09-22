0c882524058c7af40529b0c3f9f44cdd8475f121
error56 <46536532+error56@users.noreply.github.com>
2019-09-22 13:18:13 +0200

    Update FirstTest.go

--- FirstTest.go
+++ FirstTest.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHello(t *testing.T) {
     want := "Hello, world."
-    if got := Hello(); got != want {
+    if got := "Hello, world."; got != want {
         t.Errorf("Hello() = %q, want %q", got, want)
     }
 }
